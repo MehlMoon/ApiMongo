ae6355117a49d823a4ceb00a1a5d198e28dab9ca
Elsa Aranda M <arandamendozaelsa@gmail.com>
2022-11-19 11:24:45 -0600

    Agregar archivos de proyecto.

---
+++ ApiMongo/Controllers/AlumnoController.cs
@@ -0,0 +1,48 @@
+﻿using ApiMongo.Models;
+using ApiMongo.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ApiMongo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlumnoController : ControllerBase
+    {
+        public EscuelaService _alumnoService;
+
+        public AlumnoController(EscuelaService alumnoService)
+        {
+            _alumnoService = alumnoService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Alumnos>> Get()
+        {
+            return _alumnoService.Get();
+        }
+
+        [HttpPost]
+        public ActionResult<Alumnos> Create(Alumnos alumnos)
+        {
+            return _alumnoService.Create(alumnos);
+        }
+
+
+        [HttpPut]
+        public ActionResult Update(Alumnos alumnos)
+        {
+            _alumnoService.Update(alumnos.Id, alumnos);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(string id)
+        {
+            _alumnoService.Delete(id);
+            return Ok();
+        }
+
+    }
+}
---
+++ ApiMongo/Services/EscuelaService.cs
@@ -0,0 +1,46 @@
+﻿using MongoDB.Driver;
+using ApiMongo.Models;
+using Microsoft.OpenApi.Any;
+using System.Collections.Generic;
+
+namespace ApiMongo.Services
+{
+    public class EscuelaService
+    {
+
+        private IMongoCollection<Alumnos> _alumnos;
+
+
+        public EscuelaService(IEscuelaSettings settings)
+        {
+            var cliente = new MongoClient(settings.Server);
+            var database = cliente.GetDatabase(settings.Database);
+            _alumnos = database.GetCollection<Alumnos>(settings.Collection);
+        }
+
+        public List<Alumnos> Get()
+        {
+            return _alumnos.Find(d => true).ToList();
+        }
+
+        public Alumnos Create(Alumnos alumnos)
+        {
+            _alumnos.InsertOne(alumnos);
+            return alumnos;
+        }
+
+        public void Update(string id, Alumnos alumnos)
+        {
+            _alumnos.ReplaceOne(alumnos => alumnos.Id == id, alumnos);
+        }
+
+
+        public void Delete(string id)
+        {
+            _alumnos.DeleteOne(d => d.Id == id);
+        }
+
+
+
+    }
+}
